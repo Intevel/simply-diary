6dcf011ebf470e1a8e29fcb33433530d23653836
Intevel ツ <59223342+Intevel@users.noreply.github.com>
2021-09-11 10:00:16 +0200

    Create new-hero.vue

---
+++ components/new-hero.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="mb-3 mt-10">
+    <div class="bg-gray-100">
+      <div class="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
+        <div class="max-w-xl mb-4 md:mx-auto sm:text-center lg:max-w-2xl md:mb-6">
+          <div>
+            <p
+              class="inline-block px-3 py-1 mb-4 text-xs font-bold tracking-wider text-white uppercase rounded-full bg-simplydiary-primary">
+              Simply Diary</p>
+          </div>
+          <h2
+            class="max-w-lg mb-6 font-sans text-6xl font-bold leading-none tracking-tight text-gray-900 sm:text-6xl md:mx-auto">
+            Create your Diary, please enter the Diary Title
+          </h2>
+        </div>
+        <new-form />
+      </div>
+    </div>
+
+  </div>
+</template>
