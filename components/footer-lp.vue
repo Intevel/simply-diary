8fe78273939067c25978a60cd6b5db0c2072a223
Intevel ツ <59223342+Intevel@users.noreply.github.com>
2021-09-11 10:18:05 +0200

    Create footer-lp.vue

---
+++ components/footer-lp.vue
@@ -0,0 +1,31 @@
+<template>
+<div class="px-4 pt-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8">
+  <div class="flex flex-col justify-between pt-5 pb-10 border-t sm:flex-row">
+    <p class="text-sm text-gray-600 font-nunito">
+      © Copyright 2021 Conner Bachmann. All rights reserved.
+    </p>
+    <div class="flex items-center mt-4 space-x-4 sm:mt-0">
+      <a href="https://twitter.com/connercodes/" class="text-gray-500 transition-colors duration-300 hover:text-simplydiary-primary">
+        <svg viewBox="0 0 24 24" fill="currentColor" class="h-5">
+          <path
+            d="M24,4.6c-0.9,0.4-1.8,0.7-2.8,0.8c1-0.6,1.8-1.6,2.2-2.7c-1,0.6-2,1-3.1,1.2c-0.9-1-2.2-1.6-3.6-1.6 c-2.7,0-4.9,2.2-4.9,4.9c0,0.4,0,0.8,0.1,1.1C7.7,8.1,4.1,6.1,1.7,3.1C1.2,3.9,1,4.7,1,5.6c0,1.7,0.9,3.2,2.2,4.1 C2.4,9.7,1.6,9.5,1,9.1c0,0,0,0,0,0.1c0,2.4,1.7,4.4,3.9,4.8c-0.4,0.1-0.8,0.2-1.3,0.2c-0.3,0-0.6,0-0.9-0.1c0.6,2,2.4,3.4,4.6,3.4 c-1.7,1.3-3.8,2.1-6.1,2.1c-0.4,0-0.8,0-1.2-0.1c2.2,1.4,4.8,2.2,7.5,2.2c9.1,0,14-7.5,14-14c0-0.2,0-0.4,0-0.6 C22.5,6.4,23.3,5.5,24,4.6z">
+          </path>
+        </svg>
+      </a>
+      <a href="https://github.com/cnrycodes/simply-diary/" class="text-gray-500 transition-colors duration-300 hover:text-simplydiary-primary">
+        <svg viewBox="0 0 16 16" fill="currentColor" class="h-5">
+          <path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.012 8.012 0 0 0 16 8c0-4.42-3.58-8-8-8z"/>
+        </svg>
+      </a>
+    </div>
+  </div>
+</div>
+</template>
+<style>
+  @import url('https://fonts.googleapis.com/css2?family=Nunito&display=swap');
+
+  .font-nunito {
+    font-family: "Nunito", sans-serif !important;
+  }
+
+</style>
